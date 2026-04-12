cb19552bb96f1c61579c4ad5bc402853744d498e
Asad Qureshi <asad.qureshi08@gmail.com>
2026-04-13 00:51:29 +0530

    Milestone 1

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,6 +1,5 @@
 // import logo from './logo.svg';
 import "./App.css";
-import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import { BrowserRouter } from "react-router-dom";
